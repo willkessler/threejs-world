b16bdef1d5d620fe80970e17444a3a75677ea44c
Will Kessler <willkessler@gmail.com>
2017-12-07 21:39:55 -0800

    trying to work out camPositions code

--- js/landscape.js
+++ js/landscape.js
@@ -58,13 +58,18 @@ var createWorld = (options) => {
   const width = options.width || 200;
   const height = options.height || 200;
   const resolution = options.resolution || 125;
-  geometry = new THREE.PlaneGeometry(width, height, resolution, resolution);
-  var vertices = geometry.vertices;
   let e,j,power;
   let maxY = -1.0e-5;
   let minY = 1.0e+5;
   const noiseDepth = 20;
   const exponent = 2;
+  let balloonPositions = {};
+  let vertex,face, balloonGeometry, balloonMaterial, balloon;
+  geometry = new THREE.PlaneGeometry(width, height, resolution, resolution);
+  var vertices = geometry.vertices;
+  balloonMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  let centerVertices = {};
+  
   for (let i in vertices) {
     vertices[i].z = vertices[i].y;
     var nx = (vertices[i].x + width/2)  / options.width ;
@@ -75,10 +80,35 @@ var createWorld = (options) => {
       power = Math.pow(2, j);
       e += 1/power * simplex.noise2D(power * nx, power * ny);
     }
-    vertices[i].y = 25 * Math.pow(e, exponent);
+    //    vertices[i].y = 25 * Math.pow(e, exponent);
+    //console.log(vertices[i].x, vertices[i].z);
+    //vertices[i].y = e * 4;
+    vertices[i].y = 0;
+    if (vertices[i].x === 0) {
+      let zSpot =  (height /2) - vertices[i].z;
+      balloonPositions[zSpot] = vertices[i].y;
+    }
     maxY = (maxY < vertices[i].y ? vertices[i].y : maxY);
     minY = (minY > vertices[i].y ? vertices[i].y : minY);
   }
+
+  let thing = 0;
+  const descendingSortedKeys = Object.keys(balloonPositions).sort(function(a,b){return a-b});
+  console.log(descendingSortedKeys);
+  let camPositions = [];
+  for (let cp of descendingSortedKeys) {
+    camPositions.push(balloonPositions[cp]);
+  }
+  for (let cp in camPositions) {
+    balloonGeometry = new THREE.BoxGeometry(2,2,.1);
+    balloon = new THREE.Mesh(balloonGeometry, balloonMaterial);
+    balloon.position.x = 0;
+    balloon.position.y = camPositions[cp] ;
+    //console.log('h',bk, balloonPositions[bk]);
+    balloon.position.z =  cp * (height / resolution) - (height / 2);
+    options.group.add(balloon);
+  }
+  
   const yRange = maxY - minY;
   const snowRange = yRange / 8;
   const beachRange = yRange / 25;
@@ -88,9 +118,10 @@ var createWorld = (options) => {
   const beach = 0xffff00;
   const grass = 0x00dd00;
 
-  let vertex;
+  let faceCtr = 0;
   for (let faceId in geometry.faces) {
-    vertex = geometry.vertices[geometry.faces[faceId].a];
+    face = geometry.faces[faceId];
+    vertex = geometry.vertices[face.a];
     if (vertex.y > maxY - snowRange) {
       geometry.faces[faceId].color.setHex(snow);
     } else if (vertex.y < minY + beachRange) {
@@ -98,11 +129,18 @@ var createWorld = (options) => {
     } else {
       geometry.faces[faceId].color.setHex(grass);
     }
+    face.centroid = new THREE.Vector3( 0, 0, 0 );
+    face.centroid.add( geometry.vertices[ face.a ] );
+    face.centroid.add( geometry.vertices[ face.b ] );
+    face.centroid.add( geometry.vertices[ face.c ] );
+    face.centroid.divideScalar( 3 );
+    faceCtr = (faceCtr == resolution ? 0 : faceCtr + 1);
   }
-  const material = new THREE.MeshLambertMaterial({side: THREE.DoubleSide, vertexColors: THREE.FaceColors });
+  const material = new THREE.MeshLambertMaterial({side: THREE.DoubleSide, /*wireframe:true,*/ vertexColors: THREE.FaceColors });
   const ground = new THREE.Mesh(geometry, material);
   geometry.computeVertexNormals();
   ground.geometry.colorsNeedUpdate = true;
+//  ground.position.z = -50;
 
   const oceanGeometry = new THREE.PlaneGeometry(width,height,1,1);
   const oceanVertices = oceanGeometry.vertices;
@@ -113,17 +151,21 @@ var createWorld = (options) => {
 
   // https://stackoverflow.com/questions/44749446/enable-smooth-shading-with-three-js
   // https://stackoverflow.com/questions/15994944/transparent-objects-in-threejs
-  oceanMaterial = new THREE.MeshBasicMaterial({color: 0x1155dd, side: THREE.DoubleSide, transparent: true, opacity: 0.7});
+  oceanMaterial = new THREE.MeshBasicMaterial({color: 0x1155dd, side: THREE.DoubleSide, transparent: true, opacity: 0.85});
   const ocean = new THREE.Mesh(oceanGeometry, oceanMaterial);
 
   options.group.add(ground);
-  options.group.add(ocean);
-  makeText('Udacity', options.group);
+  //options.group.add(ocean);
+  //makeText('Udacity', options.group);
   options.scene.add(options.group);
 
   return ({
+    width: width,
+    height: height,
+    resolution: resolution,
     ground: ground,
-    ocean: ocean
+    ocean: ocean,
+    camPositions: balloonPositions
   });
     
 };
@@ -134,8 +176,12 @@ function render() {
   cam_posy = (camera.position.y < 5 ? -cam_posy : cam_posy);
   cam_posy = (camera.position.y > 10 ? -cam_posy : cam_posy);
   cam_posy = (Math.abs(camera.position.y) > 0.2 ? -cam_zrot: cam_zrot);
-
-  group.position.z += 0.05;
+  
+  //group.position.z += 0.01;
+  let index = Math.floor(group.position.z / scenery.height * scenery.resolution);
+  let camY = scenery.camPositions[index];
+  //console.log('z:',group.position.z, 'index:', index, 'camY:', camY);
+  //camera.position.y = camY;
   //  console.log('Group Z:', group.position.z);
   //  camera.position.y += 0.08;
   //  camera.rotation.z += cam_zrot;
@@ -155,7 +201,7 @@ function render() {
 
 var simplex = new SimplexNoise();
 var scene = new THREE.Scene();
-scene.fog = new THREE.FogExp2( 0xffffff, 0.0075 );
+//scene.fog = new THREE.FogExp2( 0xffffff, 0.0075 );
 
 var group = new THREE.Group();
 
@@ -171,9 +217,10 @@ scene.add( light );
 
 console.log('Beginning world build.');
 const startTime = new Date().getTime();
-const width = 200;
+//const width = 200;
+const width = 100;
 const height = width;
-const resolution = 125;
+const resolution = 10;
 const scenery = 
   createWorld({
     width: width,
@@ -193,7 +240,8 @@ renderer.setClearColor(0xffffff, 1)
 document.body.appendChild(renderer.domElement);
 
 
-camera.position.z = height/4;
+//camera.position.z = 5;
+camera.position.z = height - height /4;
 camera.position.y = 10;
 let cam_posy = -0.5;
 const cam_zrot = -0.0015;
@@ -204,5 +252,5 @@ console.log('numSquares:', numSquares, 'midSquare:', midSquare);
 let camYMax = scenery.ground.geometry.vertices[midSquare].y;
 console.log('yMax:', camYMax);
 
-render();
+render(scenery);
 getLorem();
